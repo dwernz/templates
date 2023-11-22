7bc4455a24062e33774c8dcd4358c8ee4a29d28f
Daniel <62781672+dwernz@users.noreply.github.com>
2023-11-21 20:22:28 -0500

    initial

--- src/components/buttons/bootstrap-button/bootstrap-button.component.jsx
+++ src/components/buttons/bootstrap-button/bootstrap-button.component.jsx
@@ -1,8 +1,19 @@
 import { Button } from "./bootstrap-button.styles";
 
-const BootstrapButton = ({text, theme, color, size}) => {
+/*
+    text specifies the text the button will display.
+
+    color defines the colors.
+        current list: primary, secondary, success, danger, warning, info, light, dark
+
+    size determines the size of the button.
+        current list: extra-small, small, default, large, extra-large
+        note: default does not need to be specified.
+*/
+
+const BootstrapButton = ({text, color, size}) => {
     return (
-        <Button theme={theme} className={`${color} ${size}`}>
+        <Button className={`${color} ${size}`}>
             {text}
         </Button>
     )
