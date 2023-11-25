ec9d12135a122d7801f3fdfa1f991b242cb49991
Daniel <62781672+dwernz@users.noreply.github.com>
2023-11-24 21:30:13 -0500

    update: minor fixes

--- src/routes/myprofile/social-icon/social-icon.component.jsx
+++ src/routes/myprofile/social-icon/social-icon.component.jsx
@@ -4,27 +4,27 @@ const SocialIcon = () => {
     return (
         <SocialIconContainer>
             <li>
-                <a href='#'>
+                <a href='https://www.facebook.com'>
                     <i className="fa-brands fa-facebook"></i>
                 </a>
             </li>
             <li>
-                <a href='#'>
+                <a href='https://www.twitter.com'>
                     <i className="fa-brands fa-x-twitter"></i>
                 </a>
             </li>
             <li>
-                <a href='#'>
+                <a href='https://www.linkedin.com'>
                     <i className="fa-brands fa-linkedin"></i>
                 </a>
             </li>
             <li>
-                <a href='#'>
+                <a href='https://www.slack.com'>
                     <i className="fa-brands fa-slack"></i>
                 </a>
             </li>
             <li>
-                <a href='#'>
+                <a href='https://www.instagram.com'>
                     <i className="fa-brands fa-instagram"></i>
                 </a>
             </li>
